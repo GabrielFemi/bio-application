aa0793b4906166fb6d012ac14e80a3f674ad395a
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-01-23 09:37:51 +0100

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Bio application',
       theme: ThemeData(
-        fontFamily: 'Poppins', // This is the theme of your application.
+        fontFamily: 'GoogleSans', // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
